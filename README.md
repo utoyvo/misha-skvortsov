db367441db480a8030782cf912c6390b62f3caec
Oleksandr Klochko <utoyvo@protonmail.com>
2020-05-28 22:00:00 +0300

    update

--- README.md
+++ README.md
@@ -4,32 +4,33 @@ Where I took the first steps in mastering the virtual space. It's a web site for
 
 ![Misha Skvortsov](screenshot.png)
 
-
 ## STRUCTURE
 ```
 ├── assets
 │   ├── css
 |   |   └── style.css
 │   ├── img
+|   |   ├── branding
+|   |   |   ├── brand.png
+|   |   |   └── logo-192x192.png
 |   |   ├── covers
 |   |   |   ├── cover-1200x675.jpg
 |   |   |   └── cover-1280x800.jpg
-|   |   ├── icons
-|   |   |   ├── icon-16x16.png
-|   |   |   ├── icon-32x32.png
-|   |   |   ├── icon-60x60.png
-|   |   |   ├── icon-76x76.png
-|   |   |   ├── icon-96x96.png
-|   |   |   ├── icon-120x120.png
-|   |   |   ├── icon-144x144.png
-|   |   |   ├── icon-152x152.png
-|   |   |   ├── icon-180x180.png
-|   |   |   └── icon-192x192.png
-|   |   └── logo
-|   |       └── logo-192x192.png
+|   |   └── icons
+|   |       ├── icon-16x16.png
+|   |       ├── icon-32x32.png
+|   |       ├── icon-60x60.png
+|   |       ├── icon-76x76.png
+|   |       ├── icon-96x96.png
+|   |       ├── icon-120x120.png
+|   |       ├── icon-144x144.png
+|   |       ├── icon-152x152.png
+|   |       ├── icon-180x180.png
+|   |       └── icon-192x192.png
 │   ├── js
 |   |   └── app.js
 │   └── libs
+|       ├── aframe-1.0.4.min.js
 |       └── jquery-3.5.1.min.js
 ├── favicon.png
 ├── index.html
@@ -38,13 +39,12 @@ Where I took the first steps in mastering the virtual space. It's a web site for
 └── screenshot.png
 ```
 
-## Built With
 ## BUILT WITH
-- [A-Frame](https://aframe.io/)
+- [A-Frame 1.0.4](https://github.com/aframevr/aframe/releases/tag/v1.0.4)
 - [jQuery 3.5.1](https://github.com/jquery/jquery/releases/tag/3.5.1)
 
 ## CONTRIBUTOR
-Oleksandr Klochko / [@utoyvo](https://github.com/utoyvo).
+Oleksandr Klochko [@utoyvo](https://github.com/utoyvo).
 
 ## LICENSE
 Code released under the [MIT](LICENSE).
---
+++ public/assets/js/app.js
@@ -0,0 +1,151 @@
+
+var planes     = 11,
+	z_position = 99;
+
+// Camera position
+var camera_position;
+AFRAME.registerComponent('listiner', {
+	tick: function () {
+		camera_position = this.el.getAttribute('position');
+	}
+});
+
+// Cursor action listener
+AFRAME.registerComponent('cursor-listener', {
+	init: function () {
+		this.el.addEventListener('mouseenter', function(evt) {
+			z_position--;
+			if (z_position < 9) {
+				z_position = 99;
+			}
+
+			this.setAttribute('position', '0 0 -4.' + z_position);
+			this.setAttribute('rotation', '0 0 0');
+
+			this.removeAttribute('cursor-listener');
+			this.setAttribute('show-listener', '');
+		});
+	}
+});
+
+// Cursor action slider
+AFRAME.registerComponent('show-listener', {
+	init: function () {
+		this.el.addEventListener('mouseup', function(evt) {
+			var position = randomPosition(-16, 8),
+				rotation = randomRotation();
+
+			this.setAttribute('position', position);
+			this.setAttribute('rotation', rotation);
+
+			this.setAttribute('cursor-listener', '');
+			this.removeAttribute('show-listener');
+		});
+	}
+});
+
+/**
+ * Preload Items
+ */
+function preloadItems() {
+	var assets = document.querySelector('a-assets'),
+		items = [];
+
+	for (var i = 1; i < planes; i++) {
+		items[i] = document.createElement('img');
+		items[i].setAttribute('id', 'item-' + i);
+		items[i].setAttribute('src', 'public/uploads/' + i + '.jpg');
+
+		assets.appendChild(items[i]);
+	}
+}
+
+/**
+ * Populate Sphere
+ */
+function populateSphere() {
+	var sky = document.querySelector('a-scene');
+	for (var i = 1; i < planes; i++) {
+		var position = randomPosition(-8, 8),
+			rotation = randomRotation();
+
+		item = document.createElement('a-image');
+
+		item.setAttribute('src', '#item-' + i);
+
+		item.setAttribute('position', position);
+		item.setAttribute('rotation', rotation);
+		item.setAttribute('scale', '8 8 8');
+
+		item.setAttribute('cursor-listener', '');
+
+		sky.appendChild(item);
+	}
+}
+
+/**
+ * Observe position
+ */
+function observePosition() {
+	var cursor = document.getElementById('cursor');
+
+	var	x = cursor.x,
+		y = cursor.y,
+		z = cursor.z;
+
+	var numbers = {};
+
+	alert(Object.keys(cursor));
+	alert(Object.values(cursor));
+
+	numbers.x = x;
+	numbers.y = y;
+	numbers.z = z;
+
+	return numbers;
+}
+
+/**
+ * Random Position
+ */
+function randomPosition(min = 0, max = 1) {
+	var numbers = {},
+
+		x = Math.floor(Math.random() * (max - min + 1)) + min,
+		y = Math.floor(Math.random() * (max - min + 1)) + min,
+		z = Math.floor(Math.random() * (max - min + 1)) + min;
+
+	numbers.x = x;
+	numbers.y = y;
+	numbers.z = z;
+
+	return numbers;
+}
+
+/**
+ * Random Rotation
+ */
+function randomRotation(min = 0, max = 180) {
+	var numbers = {},
+		x = Math.floor(Math.random() * (max - min + 1) + min),
+		y = Math.floor(Math.random() * (max - min + 1) + min),
+		z = Math.floor(Math.random() * (max - min + 1) + min);
+
+	numbers.x = x;
+	numbers.y = y;
+	numbers.z = z;
+
+	return numbers;
+}
+
+/**
+ * Setup
+ */
+function setup() {
+	preloadItems();
+	populateSphere();
+	randomPosition();
+	randomRotation();
+}
+
+setup();
